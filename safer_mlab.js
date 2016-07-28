1b7ab475768d77c53cb8591e6bede66a6fc31bda
Rich Unger <runger@lyft.com>
2016-07-28 14:06:20 -0700

    initial checkin

---
+++ safer_mlab.js
@@ -0,0 +1,5 @@
+jq = jQuery.noConflict(true);
+jq('.delete').hide();
+jq('a:contains("Delete cluster")').hide();
+jq('a:contains("Delete database")').hide();
+jq('#deleteAllCollectionsButton').hide();
